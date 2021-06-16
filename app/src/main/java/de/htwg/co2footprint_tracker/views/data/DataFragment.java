041bd15f24000157e8cb0be0e789a9ddc9caeabe
benvae <b.vaethjunker@gmail.com>
2021-06-16 18:00:14 +0200

    Add mode toggle

--- app/src/main/java/de/htwg/co2footprint_tracker/views/data/DataFragment.java
+++ app/src/main/java/de/htwg/co2footprint_tracker/views/data/DataFragment.java
@@ -1,6 +1,7 @@
 package de.htwg.co2footprint_tracker.views.data;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,6 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.HashSet;
 
@@ -20,17 +20,19 @@ import de.htwg.co2footprint_tracker.databinding.FragmentDataBinding;
 import de.htwg.co2footprint_tracker.helpers.PackageHelper;
 import de.htwg.co2footprint_tracker.model.Co2Equivalent;
 import de.htwg.co2footprint_tracker.model.MainCardModel;
+import de.htwg.co2footprint_tracker.utils.Constants;
 
 public class DataFragment extends Fragment {
-    
+
+    FragmentDataBinding binding;
+    DatabaseHelper databaseHelper;
+
     /**
-     * Returns a new instance of the tips fragment.
+     * Returns a new instance of the data fragment.
      */
     public static DataFragment getInstance() {
         return new DataFragment();
     }
-
-    FragmentDataBinding binding;
 
     @Nullable
     @Override
@@ -48,16 +50,38 @@ public class DataFragment extends Fragment {
         binding.co2EquivalentList.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
         DataRecyclerViewAdapter dataRecyclerViewAdapter = new DataRecyclerViewAdapter(getContext(), Co2Equivalent.getEquivalents());
         binding.co2EquivalentList.setAdapter(dataRecyclerViewAdapter);
+
+        binding.modeToggle.toggle.setOnCheckedChangeListener((group, checkedId) -> {
+            switch (checkedId) {
+                case R.id.today_toggle:
+                    Log.e(Constants.LOG.TAG, "Toggle switched to: today");
+                    bindTodayData();
+                    break;
+                case R.id.total_toggle:
+                    Log.e(Constants.LOG.TAG, "Toggle switched to: total");
+                    bindTotalData();
+                    break;
+            }
+        });
+
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        DatabaseHelper databaseHelper = DatabaseHelper.getInstance(getContext());
+        databaseHelper = DatabaseHelper.getInstance(getContext());
+        bindTodayData();
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void bindTodayData() {
         HashSet<Integer> applicationUids = PackageHelper.getApplicationUids(getContext());
         final long totalReceivedBytes = databaseHelper.getTotalReceivedBytes(applicationUids);
         final double totalEnergyConsumption = databaseHelper.getTotalEnergyConsumption(applicationUids);
         binding.setMainCardModel(new MainCardModel(totalReceivedBytes, totalEnergyConsumption));
-        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void bindTotalData() {
+        // TODO bind total data like in today data
     }
 }
